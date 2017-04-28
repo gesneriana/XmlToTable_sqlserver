818407be126a902945c4fe2a46d21d0e36457983
gesneriana <s694060865@163.com>
2017-04-28 17:20:06 +0800

    添加调用范例

---
+++ xmlToTable/example.sql
@@ -0,0 +1,19 @@
+﻿use sz6_cus
+go
+declare @xml xml
+set @xml=(select top 1 * from DecHead for xml path('DecHead'))
+
+select * from dbo.XmlToTable(@xml,'DecHead')
+
+
+
+--在Sql Server中执行这段代码可以开启CLR
+exec sp_configure 'show advanced options', '1';
+go
+reconfigure;
+go
+exec sp_configure 'clr enabled', '1'
+go
+reconfigure;
+exec sp_configure 'show advanced options', '0';
+go
